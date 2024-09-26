7882737bc51159e4dc84da9b09158b1582b0ae06
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-26 08:29:32 -0500

    feat(entity): add entities validated by hibernate

---
+++ src/main/java/com/bootcamp/pragma/transactionmicroservice/infrastructure/driven/jpa/mysql/entity/SupplyEntity.java
@@ -0,0 +1,32 @@
+package com.bootcamp.pragma.transactionmicroservice.infrastructure.driven.jpa.mysql.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "supply")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class SupplyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+    @Column(name = "article_id", nullable = false)
+    private Long articleId;
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+    @Column(name = "_date")
+    private LocalDateTime date;
+
+}
