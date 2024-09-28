a6d641e0bb946fda7ba9b239110f82a92dbdcd74
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-28 14:41:38 -0500

    refactor(main): enable feign clients

--- src/main/java/com/bootcamp/pragma/transactionmicroservice/TransactionMicroserviceApplication.java
+++ src/main/java/com/bootcamp/pragma/transactionmicroservice/TransactionMicroserviceApplication.java
@@ -1,9 +1,13 @@
 package com.bootcamp.pragma.transactionmicroservice;
 
+import com.bootcamp.pragma.transactionmicroservice.infrastructure.driven.jpa.mysql.feign.IStockServiceFeign;
+import com.bootcamp.pragma.transactionmicroservice.infrastructure.driven.jpa.mysql.feign.IUserServiceFeign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(clients = {IStockServiceFeign.class, IUserServiceFeign.class})
 public class TransactionMicroserviceApplication {
 
     public static void main(String[] args) {
